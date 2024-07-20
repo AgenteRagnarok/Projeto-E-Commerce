e5bab5d045ebf36fa21f23cc416ceb3a9e945ec8
Vinicius <xyvinicius@gmail.com>
2024-07-20 17:05:52 -0300

    fetch: finalização da instalação do tailwind

--- Projeto/src/components/Navbar/index.tsx
+++ Projeto/src/components/Navbar/index.tsx
@@ -1,3 +1,7 @@
-export const Navbar = () => {   
-    return <>Navbar</>;
+export const Navbar = () => {
+    return (
+        <div className="flex-itens-center justify-center bg-[#000000]">   
+            <h1 className="text-primary text-6xl font-bold">Hello World!</h1>;
+        </div>
+    )
 };
--- Projeto/tailwind.config.js
+++ Projeto/tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
+  content: ["./src/**/*.{html,js,jsx,ts,,tsx}"],
+  theme: { colors: {
+    primary: "#C92071",
+    secondary: "#B5B6F2",
+    tertiary: "#4F0F24",
+    error: "#EE4266",
+    success: "#52CA76",
+    warning: "#F6AA1C",
+    info: "#3C7CFF",
+    primaryBackground: "linear-gradient(90deg, #C92071 0%, #EDABCA 100%)",
+    secondaryBackground: "linear-gradient(90deg, #B5B6F2 0%, #EFEFFF 100%)"
+    
+  },
     extend: {},
   },
   plugins: [],
